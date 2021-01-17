d6c979f7048fb5f723b3eed66ac615da81adb367
Chris <chriscar.sam@gmail.com>
2021-01-17 11:42:16 -0500

    Múltiples importaciones y exportaciones por defecto

--- src/data/heroes.js
+++ src/data/heroes.js
@@ -1,5 +1,5 @@
 
-export const heroes = [
+const heroes = [
     {
         id: 1,
         name: 'Batman',
@@ -26,3 +26,12 @@ export const heroes = [
         owner: 'Marvel'
     },
 ];
+
+export const owners = ['DC', 'Marvel'];
+
+export default heroes;
+
+// export {
+//     heroes as default,
+//     owners
+// }
